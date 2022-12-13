645bfd77a15cba22f12c284f03b56decbbbe096a
anais-gilg <114387786+anais-gilg@users.noreply.github.com>
2022-12-13 16:52:13 +0100

    Index booking

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+
+    require("include/dbconnect.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Home</title>
+    </head>
+
+    <body class="bcgindex">
+        <!--Header-->
+        <?php require("include/header.php") ?>
+
+        <main>
+            
+
+            <div class="welcome">
+                <?php if ($userConnected) {?>
+                    
+                    <h1>Welcome to your agenda, <br><?php echo $login ?> ! </h1>
+                    
+                <?php } else {?>
+                    <h1>Online agenda</h1>
+                <?php } ?>
+            </div>
+        </main>
+
+        <?php require("include/footer.php")?>
+
+    </body>
+
+</html>
